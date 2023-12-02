ff0b22c3e0f3aea1f2f940870fbd0d63b8cc7331
RahulJana <janarahul1234@gmail.com>
2023-12-03 01:03:22 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const popup = document.getElementById("popup");
+
+function showPopup(){
+    popup.style.visibility = "visible";
+}
+
+function hidePopup(){
+    popup.style.visibility = "hidden";
+}
